e00292be454cdb2a64f321f8ce24beb6e10010b2
Julius <njmwas@gmail.com>
2023-09-29 11:24:42 +0300

    Mongo credentials secured

--- _config.js
+++ _config.js
@@ -1,9 +1,8 @@
 var config = {}
-// 'mongodb+srv://njmwas:Lk6DF5OeyJQowmwa@devops-ip-01.sazurxy.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://njmwas:Lk6DF5OeyJQowmwa@devops-ip-01.sazurxy.mongodb.net/devops-ip01-gallery?retryWrites=true&w=majority&appName=AtlasApp',
+    production: `mongodb+srv://${process.env.MONGO_DB_USERNAMECOLPASSWORD}@devops-ip-01.sazurxy.mongodb.net/devops-ip01-gallery?retryWrites=true&w=majority&appName=AtlasApp`,
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
